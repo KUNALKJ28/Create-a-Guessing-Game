413a0dfd7dc23a230261bef9f9cdbfb69f944bff
Kunal Sandeep Jadhav <kunaljadhav643@gmai>
2025-11-24 19:14:05 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import random
+
+def main():
+    # Generate a random number between 1 and 100
+    secret_number = random.randint(1, 100)
+    attempts = 0
+    
+    print("Welcome to the Guessing Game!")
+    print("I'm thinking of a number between 1 and 100. Try to guess it!")
+    
+    while True:
+        try:
+            guess = int(input("Enter your guess: "))
+            attempts += 1
+            
+            if guess < secret_number:
+                print("Too low! Try again.")
+            elif guess > secret_number:
+                print("Too high! Try again.")
+            else:
+                print(f"Congratulations! You guessed it in {attempts} attempts.")
+                break
+        except ValueError:
+            print("Please enter a valid integer.")
+
+if __name__ == "__main__":
+    main()
